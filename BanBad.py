516d0c8a68d815e61ada51661897412e0adcb100
leoweyr <2516427163@qq.com>
2022-04-15 10:30:34 +0800

    :<(Bad coding experience on mobile)

--- BanBad.py
+++ BanBad.py
@@ -9,9 +9,23 @@ class ExistingStation:
         self.Y = positionY
 
 #现有基站对象类
+
 #exec("{} = Program(\"{}\")".format(programName, packageList.getCellValue(row, column_program)))
 def GetExistingStation:
     with open("./TopicData/附件2 现网站址坐标(筛选).csv",encoding="utf-8") as csv_existingStation:
         reader = csv_existingStation.reader(csv_existingStation)
-        for i in reader:
-            exec()
+        rowID = 0
+        for row in reader:
+            if(rowID != 0):
+                cellID = 0
+                for cell in row:
+                    if(cell == 0):
+                        ExistingStation_id = cell
+                    elif(cell == 1):
+                            ExistingStation_x = cell
+                    elif(cell == 2):
+                        ExistingStation_y = cell
+                    cell += 1
+                exec()
+                        
+            row += 1
