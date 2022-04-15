1d95b953c06d1c7ff96a498d1986c52cf7faa3bc
leoweyr <2516427163@qq.com>
2022-04-15 18:44:52 +0800

    Complete ConditionJudge.py

--- ConditionJudge.py
+++ ConditionJudge.py
@@ -1,40 +1,24 @@
 #本程序作用：收录所以要用到的条件判断操作
 
-def BanCoordinate(positionX,positionY):
-#基于门限这一限定条件，输入某坐标，输出不能建站的坐标集
-    THRESHOLD = 10#门限值，为常量
-    '''
-    #坐标集square获取
-    positionGather_square = []
-    catchX = positionX + THRESHOLD
+import math
+
+def BanCoordinate(positionX,positionY): #基于门限这一限定条件，输入某坐标，输出不能建站的坐标集
+    THRESHOLD = 10 #门限值，为常量
+    positionGather_square = [] #外切于覆盖圆的正方形区域坐标集square
+    catchX = positionX - THRESHOLD
     catchY = positionY - THRESHOLD
     while (catchY <= positionY + THRESHOLD):
-        if(catchX != positionX and catchY != positionY):
+        if(catchX != positionX and catchY != positionY and catchX >= 0 and catchX <= 2499 and catchY >=0 and catchY <= 2499):
             position = [catchX,catchY]
             positionGather_square.append(position)
-        catchX -= 1
-        if (catchX = positionX - THRESHOLD):
+        if(catchX == positionY + THRESHOLD):
             catchY += 1
-            catchX = positionX + THRESHOLD
-    '''
-    banPositionGather = []
-    #圆形在坐标值皆为整数的坐标轴上覆盖的坐标点形成的集合图形即为像素正方形模型
-    catchX = positionX
-    catchY = positionY - THRESHOLD
-    triangleChaseX = 0#像素正方形模型X轴追赶正方形边界增进值
-    triangleChaseY = 0#像素正方形模型Y轴追赶正方形边界增进值
-    triangleSteeringwheelX = "+"#像素正方形模型X轴追赶方向
-    triangleSteeringwheelY = "+"#像素正方形模型Y轴追赶方向
-    while True:
-       if(catchX != positionX and catchY != positionY):
-            position = [catchX,catchY]
-            banPositionGather.append(position) 
-        if(triangleChaseX == triangleChaseY):
-            triangleChaseY += 1
-            catchY += 1
-        if(triangleSteeringwheelX == "+" and triangleChaseX <= triangleChaseY):
-            triangleChaseX += 1
-            
-        triangleChaseX     
-            
-    
+            catchX = positionX - THRESHOLD
+        else:
+            catchX += 1
+    positionGather_ban = [] #根据门限限定条件从坐标集square中筛选出不能建筑的坐标集ban
+    for position in positionGather_square:
+        distance = math.sqrt((position[0] - positionX)**2 + (position[1] - positionY)**2)
+        if(distance <= THRESHOLD):
+            positionGather_ban.append(position)
+    return positionGather_ban
